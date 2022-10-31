f3480c87a3d98db0f8fb8e131b87762ea2d6e58d
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2022-10-31 15:55:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const store = {
+    name: 'Flow',
+    address: {
+        country: 'Ukraine',
+        city: 'Kyiv',
+        street: 'Tarasa Shevchenko',
+        houseNumber: '13',
+    }
+};
+
+store.clients = {};
+store.clients.OleksiiDavydov = {
+    quantity: 10,
+    bonuses: 55,
+};
+store.clients.OlenaDemchyk = {
+    quantity: 88,
+    bonuses: 105,
+};
+store.clients.MikhailHitco = {
+    quantity: 20,
+    bonuses: 188,
+};
+store.clients.OlhaNevzgodova = {
+    quantity: 35,
+    bonuses: 102,
+};
+store.clients.PavloFedorenco = {
+    quantity: 57,
+    bonuses: 202,
+};
